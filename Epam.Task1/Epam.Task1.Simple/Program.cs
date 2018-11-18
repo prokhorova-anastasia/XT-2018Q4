a6c95c3971779cc96fc524400a001ee2e7dddc9f
prokhorova-anastasia <45105535+prokhorova-anastasia@users.noreply.github.com>
2018-11-18 21:10:18 +0400

    Epam

---
+++ Epam.Task1/Epam.Task1.Simple/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Epam.Task1.Simple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number: ");
+            int n = int.Parse(Console.ReadLine());
+            bool f = true;
+
+            for (int i = 2; i <= Math.Sqrt(n); i++)
+            {
+                if (n % i == 0)
+                {
+                    f = false;
+                    Console.WriteLine("The number is not simple!");
+                    break;
+                }
+            }
+
+            if (f == true)
+                Console.WriteLine("The number is simple!");
+        }
+    }
+}

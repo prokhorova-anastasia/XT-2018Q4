410ac159591c6a0acbe593ca9e069b07a8385d0b
prokhorova-anastasia <45105535+prokhorova-anastasia@users.noreply.github.com>
2018-11-19 23:41:55 +0400

    Правки

--- Epam.Task0/Epam.Task0.Square/Program.cs
+++ Epam.Task0/Epam.Task0.Square/Program.cs
@@ -4,9 +4,39 @@ namespace Epam.Task0.Square
 {
     class Program
     {
+        static void Square(int n)
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= n; j++)
+                    if ((i == n / 2 + 1) && (j == n / 2 + 1))
+                        Console.Write(' ');
+                    else Console.Write('*');
+                Console.WriteLine("\n");
+            }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter a positive, integer and odd number: ");
+            int n;
+
+            try
+            {
+                n = int.Parse(Console.ReadLine());
+                while (n < 1 || n % 1 != 0 || n % 2 == 0)
+                {
+                    Console.WriteLine("The number should be positive, integer and odd number. Enter a new one: ");
+                    n = int.Parse(Console.ReadLine());
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Error! You should enter a positive, integer and odd number. Enter a new one: ");
+                n = int.Parse(Console.ReadLine());
+            }
+
+            Square(n);
         }
     }
 }
